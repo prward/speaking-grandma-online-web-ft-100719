8dcce9d5105ad40002f10332e1019549b6b18671
Patrick Ward <patrick.r.ward@verizon.net>
2019-10-07 16:10:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,9 @@ def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
     elsif phrase.upcase
-    return 
+    return "NO, NOT SINCE 1938!"
+  end
+  
    
 
 
